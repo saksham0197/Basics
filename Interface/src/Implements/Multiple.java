182305d37952c4fdfd636aea9cb1c5ac08ace3da
saksham0197 <saksham0197@gmail.com>
2023-01-19 13:21:01 +0530

    Java Interface and Multiple Inheritance with interface

---
+++ Interface/src/Implements/Multiple.java
@@ -0,0 +1,31 @@
+package Implements;
+
+
+interface Code{
+	void doCode();   // public and abstract method
+}
+
+interface Java{
+	void learnJava();  // public and abstract method
+}
+
+
+ class Multiple implements Code,Java    // Multiple Inheritance
+ {
+	 public void doCode() {
+		 System.out.println("I am doing coding Daily to get good job.");
+	 }
+	 
+	 public void learnJava() {
+		 System.out.println("I am learning Java from Deep.");
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Multiple m = new Multiple();
+		m.doCode();
+		m.learnJava();
+
+	}
+
+}
